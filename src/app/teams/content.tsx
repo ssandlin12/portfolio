15e8d0c5224a8e6fbec8cddd112e4897ddfd23a6
ssandlin12 <ssandlin12@gmail.com>
2026-09-25 16:13:49 +0200

    Refine Fluent and Teams case study questions

--- src/app/teams/content.tsx
+++ src/app/teams/content.tsx
@@ -6,7 +6,7 @@ export default function TeamsContent() {
   return (
     <FluentContent
       title="Microsoft Teams"
-      subtitle="How do you build and evolve a design system for the world’s largest enterprise app?"
+      subtitle="How do you build an atom to template pipeline for the world's largest enterprise app?"
       linkHref="https://www.microsoft.com/en-us/microsoft-teams/group-chat-software"
       linkLabel="Microsoft Teams"
       videoSrc="/teams-hero.mp4"
